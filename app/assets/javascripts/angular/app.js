525f701737c594f0315593ddb7da260e6817852f
Dan Snider <dansnider@gmail.com>
2015-03-30 15:47:10 -0400

    hover effect engaged!

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -74,8 +74,40 @@
 					.attr('cx', function(d) { return x(d[xVar]); })
 					.attr('cy', function(d) { return 800 - y(d[yVar]); })
 					.attr('fill', function(d) { return color(d.rarity); })
+					.attr('class', function(d) { return d.breed })
+					.attr('class', 'dog-circle')
 					.attr('stroke-width', 1)
 					.attr('stroke', 'white')
+					.on('click', function(d){
+							console.log(this);
+						})
+					.on('mouseover', function(d){
+						d3.select(this)
+							.attr('fill', 'coral')
+						var xPosition = parseFloat(d3.select(this).attr('cx') + 10)
+						var yPosition = parseFloat(d3.select(this).attr('cy'))
+						svg.append('text')
+							.attr('id', 'tooltip')
+							.attr('x', xPosition)
+							.attr('y', yPosition -9)
+							.attr('text-anchor', 'middle')
+							.attr('font-family', 'sans-serif')
+							.attr('font-size', '11px')
+							.attr('font-weight', 'bold')
+							.text(d.breed);
+						// .attr('fill', 'red')
+						// .append('text')
+						// .text(this.__data__.breed)
+						// .attr('x', this.attributes.cx.textContent )
+						// .attr('y', this.attributes.cy.textContent )
+						// .attr("font-size", "11px")
+						// .attr("fill", 'black');
+					})
+					.on('mouseout', function(){
+						d3.select(this)
+							.attr('fill', function(d){ return color(this.__data__.rarity) })
+						d3.select('#tooltip').remove().transition();
+					});
 			
 			force.start()
 			force.resume()
